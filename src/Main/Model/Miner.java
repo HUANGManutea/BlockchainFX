62d017bd5f7c119678808d367931d9988aa79937
HUANG Manutea <huang.manutea@gmail.com>
2017-04-07 00:49:17 +0200

    switching to properties for miner

--- src/Main/Model/Miner.java
+++ src/Main/Model/Miner.java
@@ -1,39 +1,58 @@
 package Main.Model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
 /**
  * Created by Manutea on 07/04/2017.
  */
 public class Miner {
-    private Blockchain _blockchain;
-    private Block _workingBlock;
+    private ObjectProperty<Blockchain> _blockchain;
+    private ObjectProperty<Block> _workingBlock;
 
     public Miner(Blockchain blockchain){
-        _blockchain = blockchain;
-        _workingBlock = null;
+        _blockchain = new SimpleObjectProperty<>(blockchain);
+        _workingBlock = new SimpleObjectProperty<>();
     }
 
     public Blockchain get_blockchain() {
+        return _blockchain.get();
+    }
+
+    public ObjectProperty<Blockchain> blockchainProperty() {
         return _blockchain;
     }
 
+    public void setBlockchain(Blockchain _blockchain) {
+        this._blockchain.set(_blockchain);
+    }
+
     public Block getWorkingBlock() {
+        return _workingBlock.get();
+    }
+
+    public ObjectProperty<Block> workingBlockProperty() {
         return _workingBlock;
     }
 
     public void setWorkingBlock(Block _workingBlock) {
-        this._workingBlock = _workingBlock;
+        this._workingBlock.set(_workingBlock);
     }
 
     public void createNewBlock(){
-        _workingBlock = new Block(_blockchain.getLastBlock().getCurrHash());
+        _workingBlock = new SimpleObjectProperty<>(new Block(_blockchain.getValue().getLastBlock().getCurrHash()));
     }
 
     public void addBlock(){
-        _blockchain.addBlock(_workingBlock);
+        _blockchain.getValue().addBlock(_workingBlock.getValue());
+    }
+
+    public void addData(String data){
+        _workingBlock.getValue().addData(data);
     }
 
     public void mine(){
-        Thread minerThread = new Thread(new MinerTask(this,_workingBlock));
+        Thread minerThread = new Thread(new MinerTask(this,_workingBlock.getValue()));
         minerThread.start();
     }
 
